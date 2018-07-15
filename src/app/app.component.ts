c48abdf47cb3deda1f3239838e182e90f0cb4dfe
thomascsd <thomascsd808@gmail.com>
2018-07-15 22:56:52 +0800

    加入動畫效果

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import 'snapsvg-cjs';
+declare var Snap: any;
+declare var mina: any;
 
 @Component({
   selector: 'app-root',
@@ -6,7 +9,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
-  ngOnInit(): void {}
+  pointer: any;
+  ngOnInit(): void {
+    const rootEle = Snap('#root');
+    this.pointer = rootEle.select('#pointer');
+  }
 
-  volumeUp() {}
+  volumeDown() {
+    this.pointer.animate({ y: 270 }, 3000, mina.linear);
+  }
+
+  volumeUp() {
+    this.pointer.stop();
+    this.pointer.attr({
+      y: '-=5'
+    });
+    this.volumeDown();
+  }
 }
